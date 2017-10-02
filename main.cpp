08aa7679a33eabfc53146d1bc04d5551fdd9ffe1
Lopale505 <alopez167@mail.csuchico.edu>
2017-10-02 16:43:24 -0700

    Outline

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include "bitmap.h"
+#include<vector>
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+  // Ask user for file name of image
+  // Store user input
+  // Open File
+  // Go through row 1 of image and repeat for as many rows there are
+  // Go through column in row and repeat for how many columns(pixels)  there are
+  // Change pixel from a R,G,B image to a grayscale image from 0 - 255
+  // Create new file from output of previous step
+  return 0;
+}
