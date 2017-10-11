59cf8d2386aedd53b9270900ee8f031ac4d2c8e3
Lopale505 <alopez167@mail.csuchico.edu>
2017-10-11 16:16:43 -0700

    Progress

--- main.cpp
+++ main.cpp
@@ -19,8 +19,26 @@ int main()
   bmp = image.toPixelMatrix();
   cout << fileName << " has been loaded. It is " << bmp[0].size() <<" pixels wide and "<< bmp.size() << " pixels high." << endl;
   // Go through row 1 of image and repeat for as many rows there are
+  for (int row = 0; row < bmp.size(); row++)
+  {
+    
   // Go through column in row and repeat for how many columns(pixels)  there are
+    for (int column = 0; column < bmp[row].size(); column++)
+    {
+      rgb = bmp[row][column];
+      int sum = rgb.red + rgb.green + rgb.blue;
+      int avg = sum / 3;
   // Change pixel from a R,G,B image to a grayscale image from 0 - 255
-  // Create new file from output of previous step
+      rgb.red = avg;
+      rgb.green = avg;
+      rgb.blue = avg;
+      bmp[row][column] = rgb;
+      image.fromPixelMatrix(bmp);
+      image.save("oldTimeyPhoto.bmp");
+    }
+  }
+ 
+
+
   return 0;
 }
