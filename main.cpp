5dc7e7a101fdcae1832e51b4ccfa6f1458955e47
Lopale505 <alopez167@mail.csuchico.edu>
2017-10-13 10:32:10 -0700

    Latest change friday

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "bitmap.h"
 #include<vector>
 #include<iostream>
+#include<ctime>
 
 using namespace std;
 
@@ -9,6 +10,7 @@ int main()
   Bitmap image;
   vector <vector <Pixel> > bmp;
   Pixel rgb;
+  srand (time(NULL));
   // Ask user for file name of image
   string fileName;
   cout << "What is the name of the file you want to convert?" << endl;
@@ -18,27 +20,46 @@ int main()
   image.open(fileName);
   bmp = image.toPixelMatrix();
   cout << fileName << " has been loaded. It is " << bmp[0].size() <<" pixels wide and "<< bmp.size() << " pixels high." << endl;
+  // Ask if you want film grain on the photo
+/*  cout << "Would you like film grain on the photo? (Y/N)" << endl;
+  string choiceGrain;
+  cin >> choiceGrain;
+  if (choiceGrain == "y" || choiceGrain == "Y")
+  {
+    cout << "How much grain would you like? (0-100)" << endl;
+    int amtGrain;
+    cin >> amtGrain;
+  } 
+*/
   // Go through row 1 of image and repeat for as many rows there are
   for (int row = 0; row < bmp.size(); row++)
   {
-    
+
   // Go through column in row and repeat for how many columns(pixels)  there are
     for (int column = 0; column < bmp[row].size(); column++)
     {
       rgb = bmp[row][column];
       int sum = rgb.red + rgb.green + rgb.blue;
       int avg = sum / 3;
+  // Apply the grain to photo
+/*      if ( choiceGrain == "y" || choiceGrain == "Y" )
+      {
+        int amtGrain;
+        amtGrain = ( rand() % amtGrain);
+               cout << "amt grain is : " << amtGrain << endl;
+      }
+*/
   // Change pixel from a R,G,B image to a grayscale image from 0 - 255
       rgb.red = avg;
       rgb.green = avg;
       rgb.blue = avg;
       bmp[row][column] = rgb;
-      image.fromPixelMatrix(bmp);
-      image.save("oldTimeyPhoto.bmp");
     }
   }
+  image.fromPixelMatrix(bmp);
+  image.save("oldTimeyPhoto.bmp");
  
 
 
   return 0;
-}
+  }
